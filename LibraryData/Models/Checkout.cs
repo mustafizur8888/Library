cfa90e00871ee8d8f391beffba53afc9ca388ba9
mustafizur8888 <mustafizur8888@gmail.com>
2020-03-04 20:46:06 +0600

    Add project files.

---
+++ LibraryData/Models/Checkout.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LibraryData.Models
+{
+    public class Checkout
+    {
+        public int Id { get; set; }
+        public LibraryAsset LibraryAsset { get; set; }
+        public LibraryCard LibraryCard { get; set; }
+        public DateTime Since { get; set; }
+        public DateTime Until { get; set; }
+    }
+}
---
+++ LibraryServices/LibraryAssetService.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LibraryData;
+using LibraryData.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryServices
+{
+    public class LibraryAssetService : ILibraryAsset
+    {
+        private readonly LibraryContext _context;
+
+        public LibraryAssetService(LibraryContext context)
+        {
+            _context = context;
+        }
+
+
+        public IEnumerable<LibraryAsset> GetAll()
+        {
+            return _context.LibraryAssets
+                .Include(asset => asset.Status)
+                .Include(asset => asset.Location);
+        }
+
+        public void Add(LibraryAsset newAsset)
+        {
+            _context.Add(newAsset);
+            _context.SaveChanges();
+        }
+
+        public LibraryBranch GetCurrentLocation(int id)
+        {
+            return GetById(id).Location;
+        }
+
+        public string GetIsbn(int id)
+        {
+            if(_context.Books.Any(book=>book.Id==id))
+            {
+                return  _context.Books
+                    .FirstOrDefault(book=>book.Id==id)
+                    .ISBN;
+            }
+            else return "";
+        }
+
+        public string GetTitle(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetType(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAuthorOrDirector(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LibraryAsset GetById(int id)
+        {
+            return GetAll().FirstOrDefault(asset => asset.Id == id);
+        }
+        public string GetDeweyIndex(int id)
+        {
+            if(_context.Books.Any(book=>book.Id==id))
+            {
+                return  _context.Books
+                    .FirstOrDefault(book=>book.Id==id)
+                    .DeweyIndex;
+            }
+            else return "";
+        }
+    }
+}
